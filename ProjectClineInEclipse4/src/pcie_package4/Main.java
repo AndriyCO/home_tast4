68000849c73bf460d56e529a4773c3c1c8ed1740
AndriyCO <Aolijnyk73@gmail.com>
2017-07-21 11:08:13 +0300

    Come back on master from paralel

--- ProjectClineInEclipse4/src/pcie_package4/Main.java
+++ ProjectClineInEclipse4/src/pcie_package4/Main.java
@@ -3,7 +3,7 @@ package pcie_package4;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("after create new brunch we are again on =master=");
 
 	}
 
